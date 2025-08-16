5d66bc9b1f15b4c81443e364de0745350f79460a
bekirerenpeker <bekirerenpeker@gmail.com>
2025-08-16 20:07:07 +0300

    reverted the new feature parts on this branch

--- MovieRecommendationAPI/Data/AppDbContext.cs
+++ MovieRecommendationAPI/Data/AppDbContext.cs
@@ -1,14 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using MovieRecommendation.Data.Auth;
-using MovieRecommendation.Data.Movie;
 
 namespace MovieRecommendation.Data;
 
 public class AppDbContext : DbContext
 {
     public DbSet<UserData> Users { get; set; }
-    public DbSet<MovieData> Movies { get; set; }
-    public DbSet<CategoryData> Categories { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
--- MovieRecommendationAPI/Data/Movie/CategoryData.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieRecommendation.Data.Movie;
-
-public class CategoryData
-{
-    public Guid Id { get; set; } = Guid.Empty;
-    public string Name { get; set; } = "";
-    
-    public List<MovieData> Movies { get; set; } = [];
-}
