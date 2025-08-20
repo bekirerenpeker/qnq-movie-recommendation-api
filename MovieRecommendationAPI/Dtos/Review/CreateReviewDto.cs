b6ba93a60a4fcdaebb20d003c761b95795abfb02
bekirerenpeker <bekirerenpeker@gmail.com>
2025-08-20 12:48:33 +0300

    created dtos and an interface service

--- MovieRecommendationAPI/Dtos/Review/CreateReviewDto.cs
+++ MovieRecommendationAPI/Dtos/Review/CreateReviewDto.cs
@@ -3,7 +3,7 @@ namespace MovieRecommendation.Dtos.Review;
 public class CreateReviewDto
 {
     public int Rating {get ; set;} = 0;
-    public string ReviewText {get; set;}  = "";
+    public string Comment {get; set;}  = "";
     public Guid UserId { get; set; } = Guid.Empty;
     public Guid MovieId { get; set; } = Guid.Empty;
 }
--- MovieRecommendationAPI/Dtos/Review/ReviewDto.cs
+++ MovieRecommendationAPI/Dtos/Review/ReviewDto.cs
@@ -4,7 +4,7 @@ public class ReviewDto
 {
     public Guid Id { get; set; } =  Guid.Empty;
     public int Rating { get; set; } = 0;
-    public string ReviewText { get; set; } = "";
+    public string Comment { get; set; } = "";
     
     public Guid UserId { get; set; } = Guid.Empty;
     public Guid MovieId { get; set; } = Guid.Empty;
