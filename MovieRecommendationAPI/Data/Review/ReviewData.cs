4b2699fd58a0214ad3a1224baa87088e5311b269
bekirerenpeker <bekirerenpeker@gmail.com>
2025-08-21 11:50:43 +0300

    fixed the database a field was wrongly named

--- MovieRecommendationAPI/Data/Review/ReviewData.cs
+++ MovieRecommendationAPI/Data/Review/ReviewData.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieRecommendation.Data.Review;
 
 public class ReviewData
 {
     public Guid Id { get; set; } = Guid.Empty;
     public int Rating { get; set; } = 0;
-    public string Comment { get; set; } = "";
+    [MaxLength(300)] public string Comment { get; set; } = "";
 
     public Guid UserId { get; set; } = Guid.Empty;
     public Guid MovieId { get; set; } = Guid.Empty;
