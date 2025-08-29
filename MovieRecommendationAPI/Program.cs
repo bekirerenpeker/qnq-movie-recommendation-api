7e766822b326dc589f2eb1d9cceed9faf431664c
bekirerenpeker <bekirerenpeker@gmail.com>
2025-08-29 16:54:26 +0300

    removed the added line

--- MovieRecommendationAPI/Program.cs
+++ MovieRecommendationAPI/Program.cs
@@ -16,11 +16,6 @@ using MovieRecommendation.Services.Review;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(8080); // listen on 0.0.0.0:8080
-});
-
 // database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))
